5b72052c0d856933ce92b5b5650f94c1e92f08ce
Alexey <alexx10@mail.ru>
2022-02-22 15:12:41 +0300

    lesson_19 add method for body

--- app.rb
+++ app.rb
@@ -21,7 +21,6 @@ class App
   def handle_path(env)
     if env["PATH_INFO"] != ALLOW_PATH
       @status = 404
-      @body = "Wrong path\n"
       return false
     end
 
@@ -30,27 +29,36 @@ class App
 
   def handle_query_string(query_string)
     if query_string.start_with?("format=")
-      query_attributes = query_string.delete_prefix("format=")
-      handle_attributes(query_attributes)
+      @query_attributes = query_string.delete_prefix("format=")
+      handle_attributes(@query_attributes)
     else
       @status = 400
-      @body = "Wrong request\n"
     end
   end
 
   def handle_attributes(query_attributes)
-    prevent_attributes = query_attributes.scan(/\w+/) - TimeFormatter::TIME_FORMAT.keys.map(&:to_s)
+    @prevent_attributes = query_attributes.scan(/\w+/) - TimeFormatter::TIME_FORMAT.keys.map(&:to_s)
 
-    if prevent_attributes.any?
+    if @prevent_attributes.any?
       @status = 400
-      @body = "Unknown time format [#{prevent_attributes.join(", ")}]\n"
     else
       @status = 200
-      @body = TimeFormatter.time_by_format(query_attributes)
+    end
+  end
+
+  def body
+    if @status == 404
+      "Wrong path\n"
+    elsif @status == 400 && @prevent_attributes.any?
+      "Unknown time format [#{@prevent_attributes.join(", ")}]\n"
+    elsif @status == 400
+      "Wrong request\n"
+    else
+      TimeFormatter.time_by_format(@query_attributes)
     end
   end
 
   def response
-    Rack::Response.new(@body, @status, headers).finish
+    Rack::Response.new(body, @status, headers).finish
   end
 end
